838f12a101559081095722915640377556167f1f
gamonpo <gamonpo@outlook.com>
2020-08-01 13:27:48 -0300

    AI,PV,PBA,Dim,Art,Btn,Ptm,

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  ActivityIndicator, 
+  ProgressBarAndroid,
+  ProgressViewIOS,
+  Dimensions,
+  Alert,
+  Button,
+  Platform,
+} from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleTimeString()}`);
+  }
+
+
   return (
     <View style={styles.container}>
+      <ActivityIndicator size="large" color="#61DBFB" />
+      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5} color="#3f3f3f"/> }
+      {Platform.OS === "android" && 
+        <ProgressBarAndroid 
+          styleAttr="Horizontal"
+          indeterminate={false}
+          progress={0.5} 
+          color="#3f3f3f"
+        /> 
+      }
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click here!" onPress={onButtonPress}/>
       <StatusBar style="auto" />
+      <Text>Height: {height.toFixed(2)}</Text>
+      <Text>Width: {width.toFixed(2)}</Text>
     </View>
   );
 }
@@ -15,7 +46,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
