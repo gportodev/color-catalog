836249975213cdd8873c1c0f7e03281e16f5e017
gamonpo <gamonpo@outlook.com>
2020-08-02 09:34:17 -0300

    FlatList

--- App.js
+++ App.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
 import { 
   View, 
-  StyleSheet, 
+  StyleSheet,
+  FlatList 
 } from "react-native";
 
 import ColorButton from './components/ColorButton'
+import defaultColors from './data/defaultColors.json'
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue")
 
   return (
-    <View style={[styles.container, { backgroundColor }]}>
-      <ColorButton backgroundColor="red" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="green" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="blue" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="yellow" onPress={setBackgroundColor} />
-      <ColorButton backgroundColor="purple" onPress={setBackgroundColor} />
-    </View>
+    <FlatList style={[styles.container, { backgroundColor }]} 
+      data={defaultColors}
+      renderItem={({ item }) => {
+        return (
+          <ColorButton 
+            key={item.id} 
+            backgroundColor={item.color} 
+            onPress={setBackgroundColor} 
+          />
+        )
+      }}
+    />
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
+    display: "flex"
   }
 });
