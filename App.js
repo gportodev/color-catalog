894727b77ae066db768fe5c0cc64ff15080ecb56
gamonpo <gamonpo@outlook.com>
2020-08-02 09:23:12 -0300

    Import component

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, TouchableHighlight } from "react-native";
+import { 
+  View, 
+  StyleSheet, 
+} from "react-native";
 
-function ColorButton({ backgroundColor, onPress = f => f }) {
-  return (
-      <TouchableHighlight 
-        style={styles.button}
-        onPress={() => onPress(backgroundColor)}
-        underlayColor="orange"
-      >
-        <View style={styles.row}>
-          <View style={[styles.sample, { backgroundColor }]} />
-            <Text style={styles.buttonText}>{backgroundColor}</Text>
-        </View>
-      </TouchableHighlight>
-  )
-}
+import ColorButton from './components/ColorButton'
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue")
@@ -36,27 +26,5 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
-  },
-  button: {
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderRadius: 10,
-    alignSelf: "stretch",
-    backgroundColor: "rgba(255,255,255, .8)"
-  },
-  buttonText: {
-    fontSize: 30,
-    textAlign: "center"
-  },
-  row: {
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  sample: {
-    height: 20,
-    width: 20,
-    borderRadius: 10,
-    backgroundColor: "white"
   }
 });
